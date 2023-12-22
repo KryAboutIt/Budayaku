b5514fd6a7f714e6ed6164e1d5bf209b22e06ab5
KryAboutIt <145864578+KryAboutIt@users.noreply.github.com>
2023-12-22 21:17:45 +0700

    Delete src directory

--- src/components/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <nav class="fixed flex justify-between bg-[#101938] bg-opacity-50 w-full px-[2rem] sm:px-[80px] md:px-[118px] py-[19.5px] items-center">
-      <a href="/"><img class="w-32 sm:w-40 md:w-52 lg:w-60 xl:w-72 2xl:w-full" src="/logo.svg" alt="Logo" draggable="false"/></a>
-      <div class="text-kinda-white sm:text-[1.25rem] xl:text-2xl flex gap-6 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-16 2xl:gap-20 xl:pr-[4rem] 2xl:pr-[151.62px] opacity-90">
-        <a href="/" class="font-primary uppercase">Home</a>
-        <a href="#footer" class="font-primary uppercase">About</a>
-      </div>
-    </nav>
-</template>
